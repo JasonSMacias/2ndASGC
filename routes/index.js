dc71e05e291c120504cc02255e352dce9482a911
mzmzmz <44877972+JasonSMacias@users.noreply.github.com>
2019-03-19 22:56:39 -0400

    Set up new route/controller/model for games.

--- routes/index.js
+++ routes/index.js
@@ -1,9 +1,11 @@
 const express = require('express');
 const router = express.Router();
-const usersRoute = require("./users");
+const usersRoute = require('./users');
+const gamesRoute = require('./games');
 const path = require('path');
 
 router.use("/api/users", usersRoute);
+router.use("/api/games", gamesRoute);
 
 router.use(function(req, res) {
   res.sendFile(path.join(__dirname, "../client/build/index.html"))
